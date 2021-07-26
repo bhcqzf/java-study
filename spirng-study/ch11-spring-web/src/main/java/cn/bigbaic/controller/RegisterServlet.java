9d23c127907b92d4657e9e4b6222972781f6272c
bai <bai4246464@gmail.com>
2021-07-26 16:33:10 +0800

    初始化springmvc项目

--- spirng-study/ch11-spring-web/src/main/java/cn/bigbaic/controller/RegisterServlet.java
+++ spirng-study/ch11-spring-web/src/main/java/cn/bigbaic/controller/RegisterServlet.java
@@ -5,6 +5,7 @@ import cn.bigbaic.service.StudentService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -28,11 +29,17 @@ public class RegisterServlet extends HttpServlet {
         ApplicationContext ctx = new ClassPathXmlApplicationContext(config);*/
 
         WebApplicationContext ctx = null;
-        String key = WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE;
+/*        String key = WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE;
         Object attr = getServletContext().getAttribute(key);
         if (attr != null){
             ctx = (WebApplicationContext) attr;
-        }
+        }*/
+
+        //使用框架中的方法，获取容器对象
+        ServletContext sc = getServletContext();
+        ctx = WebApplicationContextUtils.getRequiredWebApplicationContext(sc);
+
+
         System.out.println("容器对象的信息=====>"+ctx);
         StudentService service = (StudentService) ctx.getBean("studentService");
         Student student = new Student();
